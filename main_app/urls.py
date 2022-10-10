821c3222ad4ee383d214b30a97bddfa39b7d2d01
Seth Miller <millerado@gmail.com>
2022-10-10 13:58:49 -0600

    add paths for create, update, delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('discs/', views.discs_index, name='index'),
   path('discs/<int:disc_id>/', views.discs_details, name='details'),
+  path('discs/create', views.DiscsCreate.as_view(), name='discs_create'),
+  path('disc/<int:pk>/update', views.DiscsUpdate.as_view(), name='discs_update'),
+  path('disc/<int:pk>/delete', views.DiscsDelete.as_view(), name='discs_delete'),
 ]

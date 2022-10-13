164555f3568b000cad46988b803a9856a6747eb2
Seth Miller <millerado@gmail.com>
2022-10-13 11:41:08 -0600

    impliment signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
   path('disc/<int:pk>/update', views.DiscsUpdate.as_view(), name='discs_update'),
   path('disc/<int:pk>/delete', views.DiscsDelete.as_view(), name='discs_delete'),
   path('disc/<int:pk>/add_throw', views.add_throw, name='add_throw'),
+  path('accounts/signup', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from django.shortcuts import render, redirect
 from .models import Disc
@@ -30,6 +32,20 @@ def add_throw(request, disc_id):
     new_throw.disc_id = disc_id
     new_throw.save()
   return redirect('discs_details', disc_id=disc_id)
+
+def signup(request):
+  error_message = None
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - Please try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error': error_message }
+  return render(request, 'registration/signup.html', context)
 
 class DiscsCreate(CreateView):
   model = Disc
